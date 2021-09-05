6d5d6abbdf7bbe0a1896cd47a95040d057c95f88
Mowgli Mecha <mowgli.mecha@deskera.com>
2021-09-05 20:45:21 +0800

    make it better

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,90 @@
 <template>
   <div id="app">
-    <block></block>
+    <masthead  v-view="viewHandler"></masthead>
+    <div :class="[{sticky: sticky }, 'nemo-nav']">
+      <navigtion></navigtion>
+    </div>
+
+      <mowgli-story id="ourStory"></mowgli-story>
+      <proposal  id="proposal"></proposal>
+      <entourage  id="entourage"></entourage>
+      <rsvp id="rsvp"></rsvp>
   </div>
 </template>
 
 <script>
-import Block from './components/Block.vue'
+import Masthead from './components/Masthead.vue';
+import Navigtion from './components/Navigation.vue';
+import MowgliStory from './components/Mowgli.vue';
+import Proposal from './components/Proposal.vue';
+import Entourage from './components/Entourage.vue';
+import Rsvp from './components/Rsvp.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    Block
+    Masthead,
+    Navigtion,
+    MowgliStory,
+    Proposal,
+    Entourage,
+    Rsvp
+  },
+  data: () => ({ sticky: false }),
+  methods: {
+    viewHandler (e) {
+      if(e.type == 'exit' && e.percentInView == 0 && e.percentCenter < 0) {
+        this.sticky = true
+      } else {
+        this.sticky = false
+      }
+
+    },
+    getList() {
+      let apiURL = 'http://localhost:4000/api/getEntourage';
+      axios.get(apiURL)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+  mounted() {
+
   }
+
+
 }
 </script>
 
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,800;1,900&display=swap');
+<style >
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif:ital@0;1&display=swap');
+  body {
+    margin:0;
+    padding:0;
+    font-family: 'Noto Serif TC', serif;
+  }
+
   #app {
-    font-family: 'Playfair Display', serif;
+    font-family: 'Noto Serif TC', serif;
+  }
+
+
+  .nemo-nav {
+    display: block;
+    position: relative;
+
+  }
+
+  .sticky {
+    position: -webkit-sticky;
+    position: sticky;
+    z-index: 100;
+    top: 0;
+    width: 100%;
+    background: #fff;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueYoutube from 'vue-youtube'
+import checkView from 'vue-check-view'
+import VueRellax from 'vue-rellax'
+import vuetimeline from "@growthbunker/vuetimeline"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeart,faRing,faUsers,faGifts,faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueScrollTo from 'vue-scrollto';
+
+library.add(faHeart,faGifts,faRing,faUsers,faQuestionCircle)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(VueScrollTo)
+Vue.use(VueRellax)
+Vue.use(vuetimeline , {
+  theme: "light",
+
+})
+Vue.use(checkView)
+Vue.use(VueYoutube)
 
 Vue.config.productionTip = false
+Vue.use(VueScrollTo, {
+     container: "body",
+     duration: 600,
+     easing: "ease-in-out",
+     offset:1000,
+     force: true,
+     cancelable: true,
+
+     x: false,
+     y: true
+ })
 
 new Vue({
   render: h => h(App),
